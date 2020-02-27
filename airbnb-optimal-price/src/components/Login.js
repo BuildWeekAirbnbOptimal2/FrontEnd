347a9b9bad2efe68bc637071856504df4b7a1adc
osogrizz <lhtor@gmail.com>
2020-02-26 20:28:18 -0800

    Adds initail user login component.

---
+++ airbnb-optimal-price/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text"/>
+        <input type="text"/>
+
+        <button>Log In</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login

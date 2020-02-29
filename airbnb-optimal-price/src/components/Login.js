e505a147c7f1744b24728a4427cea00bcb56d87c
osogrizz <lhtor@gmail.com>
2020-02-29 11:01:38 -0800

    Add initial styles for the login form.

--- airbnb-optimal-price/src/components/Login.js
+++ airbnb-optimal-price/src/components/Login.js
@@ -1,4 +1,36 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const FormWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  text-align: center;
+  margin: 80px auto 0;
+`
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+   
+
+  input {
+    padding: 5px;
+    margin-bottom: 10px
+  }
+
+  button {
+    width: 120px;
+    padding: 5px;
+    background: #ff3d4d;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    outline-color: orange;
+  }
+` 
 
 const Login = () => {
 
@@ -16,13 +48,14 @@ const handleSubmit = e => {
 }
  
   return (
-    <div>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column'}}>
+    <FormWrapper>
+      <FormContainer onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column'}}>
+      <h2>Sign in</h2>
         <input type="text" name="name" placeholder="username" />
-        <input type="password" name="password" />
+        <input type="password" name="password" placeholder="password"/>
         <button>Log In</button>
-      </form>
-    </div>
+      </FormContainer>
+    </FormWrapper>
   )
 }
 

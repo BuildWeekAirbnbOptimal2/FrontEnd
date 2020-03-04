ab134177206434badadc9d9957d189c7eecbeb81
osogrizz <lhtor@gmail.com>
2020-03-03 20:35:52 -0800

    Adds sign in sign up toggle.

--- airbnb-optimal-price/src/components/LogUp.js
+++ airbnb-optimal-price/src/components/LogUp.js
@@ -4,13 +4,13 @@ import Login from './Login'
 
 const LogUp = () => {
 
-  const [isLogging, setLogging] = useState(true)
+  const [isLogging, setLogging] = useState(false)
 
-  const handleChange = (e) => {
-    e.preventDefault()
-    setLogging(!isLogging)
-    console.log(isLogging)
-  }
+  // const handleChange = (e) => {
+  //   e.preventDefault()
+  //   setLogging(!isLogging)
+  //   console.log(isLogging)
+  // }
 
   return (
     <div style={{ display: 'flex', width: '100%', justifyContent: 'space-around', margin: '40px auto' }}>

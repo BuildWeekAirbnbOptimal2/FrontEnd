2482db13aa817a8f1d767eb2d3fe9b2a79ed7fff
carlo rothschild <carlorothschild@THORS-HAMMER.local>
2020-03-05 22:16:59 -0500

    Some clean up

--- airbnb-optimal-price/src/components/PropertyUpload.js
+++ airbnb-optimal-price/src/components/PropertyUpload.js
@@ -5,8 +5,7 @@ import "../index.css";
 const PropertyUpload = (props) => {
   const [image, setImage] = useState("");
   const [loading, setLoading] = useState(true);
-  // const [images, setimages] = useState('');
-  // const [cameback, setBack] = useState([]);
+  
 
   const uploadImage = e => {
     const file = e.target.files[0];
@@ -19,44 +18,10 @@ const PropertyUpload = (props) => {
       .then(response => {
         setImage(response.data.secure_url);
         console.log("image", response.data.secure_url);
-        // setimages("image",response.data.public_id);
       }, 3000)
       .then(setLoading(false))
       .catch(err => console.log("err", err));
   };
-  // const PropertyInfo = e => {
-  //   // const [getPhoto, setPhoto] = useState({});
-  //   console.log(images)
-  //   // const file = e.target.files;
-  //   const data = new FormData();
-  //   // data.append("file", file);
-  //   useEffect(
-  //     () => {
-  //       axios
-  //         .get(
-  //           `https://991455645283966:rCcThMm0RoPqdSxBMj3mba7hrVQ@api.cloudinary.com/v1_1/jorgescloud/image/list/${images}`
-  //         )
-  //         .then(response => {
-  //           console.log(response.data.results);
-  //           setBack(response.data.results);
-  //           const char = response.data.results;
-  //           console.log("char", char);
-  //         })
-  //         .catch(error => {
-  //           console.log("something went wrong", error);
-  //         });
-  //     },
-  //     [data],
-  //     3000
-  //   );
-
-  //   return (
-  //     console.log("cameback",cameback)
-  //   )
-    
-  //   }
-
-  // PropertyInfo();
   
   return (
     <div>

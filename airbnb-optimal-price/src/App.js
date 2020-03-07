8bc8e804b133ef85f0539baf7f34117ddc7d0d42
carlo rothschild <carlorothschild@thors-hammer.home>
2020-03-06 21:06:47 -0500

    Some clean up

--- airbnb-optimal-price/src/App.js
+++ airbnb-optimal-price/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 //Components
 import NavBar from "./navigation/NavBar";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
 import FormIndex from "./components/FormIndex";
 
 
@@ -11,8 +9,6 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/src/components/Login" component={Login} />
-        <Route path="/src/components/SignUp" component={SignUp} />
         <NavBar />
       </Switch>
       <FormIndex />
